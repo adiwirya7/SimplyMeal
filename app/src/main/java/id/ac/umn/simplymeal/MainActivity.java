1f91b53f815a5497beea73a3ee26e86dcd752016
Goldenbabyy <ventzicalovenia@gmail.com>
2020-04-10 18:01:46 +0700

    bottomnavigation

--- app/src/main/java/id/ac/umn/simplymeal/MainActivity.java
+++ app/src/main/java/id/ac/umn/simplymeal/MainActivity.java
@@ -1,37 +1,62 @@
 package id.ac.umn.simplymeal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import id.ac.umn.simplymeal.loginregister.LoginActivity;
-
-import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navigation;
 
-    private  TextView infoUsr;
-    private Button logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoUsr = (TextView) findViewById(R.id.display);
-        infoUsr.setText(Preferences.getLoggedInUser(getBaseContext()));
+        navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        logOut = (Button)findViewById(R.id.btnLogout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Preferences.clearLoggedInUser(getBaseContext());
-                startActivity(new Intent(getBaseContext(), LoginActivity.class));
-                finish();
+        getSupportActionBar().setTitle("Simply Meal");
+
+        loadFragment(new HomeFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    getSupportActionBar().setTitle("Simply Meal");
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_account:
+                    fragment = new AccountFragment();
+                    loadFragment(fragment);
+                    getSupportActionBar().setTitle("Profile");
+                    return true;
+                case R.id.navigation_cart:
+                    getSupportActionBar().setTitle("Shopping Cart");
+                    return true;
             }
-        });
+            return false;
+        }
+    };
 
-
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
