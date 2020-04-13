d66f43e838ff5536666935f10863207060631f51
vanessa ardelia <vanessaardelia@gmail.com>
2020-04-13 21:10:52 +0700

    recycler view menu

---
+++ app/src/main/java/id/ac/umn/simplymeal/AsianCategory.java
@@ -0,0 +1,46 @@
+package id.ac.umn.simplymeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+import id.ac.umn.simplymeal.CategoryView.AdapterCardView;
+
+public class AsianCategory extends AppCompatActivity {
+    BottomNavigationView navigation;
+    RecyclerView recyclerView;
+    AdapterCardView adapter;
+    ArrayList<String> menus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asian_category);
+
+        // HEADER
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // FOOTER NAVIGATION
+        navigation = findViewById(R.id.bottom_navigation);
+
+        menus = new ArrayList<>();
+        menus.add("First Cardview items");
+        menus.add("Second Cardview items");
+        menus.add("Third Cardview items");
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdapterCardView(this, menus);
+        recyclerView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/id/ac/umn/simplymeal/HomeFragment.java
+++ app/src/main/java/id/ac/umn/simplymeal/HomeFragment.java
@@ -1,10 +1,13 @@
 package id.ac.umn.simplymeal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
@@ -36,10 +39,40 @@ public class HomeFragment extends Fragment {
         //Pindah fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        carouselView = (CarouselView) view.findViewById(R.id.carouselView);
-        carouselView.setPageCount(sampleImages.length);
+        //ASIAN CATEGORY
+        LinearLayout asian_category = (LinearLayout) view.findViewById(R.id.asiancategory);
+        asian_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentasiancategory = new Intent(getActivity(), AsianCategory.class);
+                startActivity(intentasiancategory);
+            }
+        });
 
-        carouselView.setImageListener(imageListener);
+        //INDONESIAN CATEGORY
+        LinearLayout indonesian_category = (LinearLayout) view.findViewById(R.id.indonesiancategory);
+        indonesian_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentindonesiancategory = new Intent(getActivity(), IndonesianCategory.class);
+                startActivity(intentindonesiancategory);
+            }
+        });
+
+        //WESTERN CATEGORY
+        LinearLayout western_category = (LinearLayout) view.findViewById(R.id.westerncategory);
+        western_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentwesterncategory = new Intent(getActivity(), WesternCategory.class);
+                startActivity(intentwesterncategory);
+            }
+        });
+
+//        carouselView = (CarouselView) view.findViewById(R.id.carouselView);
+//        carouselView.setPageCount(sampleImages.length);
+
+//        carouselView.setImageListener(imageListener);
         return view;
     }
     ImageListener imageListener = new ImageListener() {
